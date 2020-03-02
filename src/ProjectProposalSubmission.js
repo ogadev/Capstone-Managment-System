5fe69edd294b07c2c4fe605c95e8dd3fcb84bf97
Zachary Dienstbier <zacharydienstbier@Zacharys-MacBook-Pro.local>
2020-03-02 10:32:13 -0700

    Working on proposal submission

--- src/ProjectProposalSubmission.js
+++ src/ProjectProposalSubmission.js
@@ -48,6 +48,7 @@ class ProjectProposalSubmission extends Component {
         //this was just in al of Oscar's messages
         event.preventDefault()   
         console.log("Hello world")
+        console.log(this.state.fProjectDescription)
       
     }
 
@@ -79,37 +80,124 @@ class ProjectProposalSubmission extends Component {
                                 placeholder="email" 
                                 value={this.state.fProposerOrg}
                                 onChange={this.onInputChange}
-                                name="pOrg"
+                                name="fProposerOrg"
                                 required
                             />
                             <label for="pName">Proposer Name</label>
                             <input 
                                 type="text" 
-                                placeholder="password" 
-                                value={this.state.password}
+                                placeholder="" 
+                                value={this.state.fProposerName}
                                 onChange={this.onInputChange}
-                                name="pName"
+                                name="fProposerName"
                                 required
                             />
-                            <h3>Proposer Email Address</h3>
-                            
-                            <h3>Project Contact Name(if different than proposer)</h3>
-                            <h3>Project Contact Email(if different than proposer)</h3>
-                            <h3>Project Title</h3>
-                            <h3>Project Description</h3>
-                            <h3>Student Learning Goals/Experience</h3>
-                            <h3>Expected Deliverables</h3>
-                            <h3>Desired Background</h3>
-                            <h3>Desired # of Groups</h3>
-                            <h3>Desired # of Group Members</h3>
-                            <h3>Interest in sponsoring a course section</h3>
+                            <label for="fProposerEmail">Proposer Email Address</label>
+                            <input 
+                                type="email" 
+                                placeholder="" 
+                                value={this.state.fProposerEmail}
+                                onChange={this.onInputChange}
+                                name="fProposerEmail"
+                                required
+                            />
+                            <label for="fProposerContactName">Project Contact Name(if different than proposer</label>
+                            <input 
+                                type="text" 
+                                placeholder="" 
+                                value={this.state.fProposerContactName}
+                                onChange={this.onInputChange}
+                                name="fProposerContactName"
+                                required
+                            />
+                            <label for="fProposerContactEmail">Project Contact Email(if different than proposer</label>
+                            <input 
+                                type="email" 
+                                placeholder="" 
+                                value={this.state.fProposerContactEmail}
+                                onChange={this.onInputChange}
+                                name="fProposerContactEmail"
+                                required
+                            />
+                            <label for="fProjectTitle">Project Title</label>
+                            <input 
+                                type="text" 
+                                placeholder="" 
+                                value={this.state.fProjectTitle}
+                                onChange={this.onInputChange}
+                                name="fProjectTitle"
+                                required
+                            />
+                            <label for="fProjectDescription">Project Description</label>
                             <textarea 
-                                name="message"
+                                name="fProjectDescription"
                                 rows="10" 
                                 cols="30" 
                                 onChange={this.onInputChange}
                                 value={this.fProjectDescription}
                             ></textarea>
+                            <br></br>
+                            <label for="fProjectExperience">Project Experience/Learning Goals</label>
+                            <textarea 
+                                name="fProjectExperience"
+                                rows="10" 
+                                cols="30" 
+                                onChange={this.onInputChange}
+                                value={this.fProjectExperience}
+                            ></textarea>
+                            <br></br>
+                            <label for="fExpectedDeliverables">Project Deliverables</label>
+                            <textarea 
+                                name="fExpectedDeliverables"
+                                rows="10" 
+                                cols="30" 
+                                onChange={this.onInputChange}
+                                value={this.fExpectedDeliverables}
+                            ></textarea>
+                            <br></br>
+                            <label for="fDesiredBackground">Desired Background</label>
+                            <textarea 
+                                name="fDesiredBackground"
+                                rows="10" 
+                                cols="30" 
+                                onChange={this.onInputChange}
+                                value={this.fDesiredBackground}
+                            ></textarea>
+                            <br></br>
+                            <label for="fDesiredNumberGroups">Desired Number of Groups</label>
+                            <input 
+                                type="range" 
+                                placeholder="" 
+                                value={this.state.fDesiredNumberGroups}
+                                onChange={this.onInputChange}
+                                name="fDesiredNumberGroups"
+                                min="1"
+                                max="5"
+                                required
+                            />
+                            <label for="fDesiredNumberMembers">Desired Number of Group Members</label>
+                            <input 
+                                type="range" 
+                                placeholder="" 
+                                value={this.state.fDesiredNumberMembers}
+                                onChange={this.onInputChange}
+                                name="fDesiredNumberMembers"
+                                min="2"
+                                max="7"
+                                required
+                            />
+                            <label for="fSemesterYear">Interest in sponsoring a course section</label>
+                            <input 
+                                type="text" 
+                                placeholder="" 
+                                value={this.state.fSemesterYear}
+                                onChange={this.onInputChange}
+                                name="fSemesterYear"
+                                required
+                            />
+                            
+                            <h3>Interest in sponsoring a course section</h3>
+                            
                             <button className="btn"> login</button>
                             
                         </form>
